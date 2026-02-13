497f07cf723806efdf3e25f53bf450191c7e7c21
CoachFroden <frode@vibliz.com>
2026-02-13 08:01:46 +0100

    Add files via upload

--- refleksjon.js
+++ refleksjon.js
@@ -1,4 +1,4 @@
-import { auth, db } from "../firebase-refleksjon.js";
+import { auth, db } from "./firebase-refleksjon.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 import {
   collection,
